b6ee0d2256fe9fd0fd95c5d63dc8cbc5f41ff0d7
Ciposki <francesco.gissara@gmail.com>
2025-05-09 17:51:53 +0200

    All :w

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
+vim.wo.relativenumber=true
 vim.opt.fillchars = {eob = " "}
 require("config.lazy")
 
